7f612f82b42fbe469ce177ef0afbe6f8f7015ba2
annagabrielsen <annagabrielsen@users.noreply.github.com>
2018-11-05 09:54:29 +0100

    Continued the work on the scroll script. Included unversioned files

--- lesMer.js
+++ lesMer.js
@@ -1,3 +1,14 @@
-var scrollElement = document.getElementById("intro");
+ const intro = document.getElementById("intro");
+    const offset = intro.offsetTop;
+    let progress = 0;
 
-document.getElementById("lesMer").onclick = scrollElement.scrollIntroView();
+function scroll() {
+    progress += 10;
+    window.scrollTo(0,progress);
+    if (progress < offset) {
+        requestAnimationFrame(scroll)
+    }
+}
+
+document.getElementById("lesMer").addEventListener("click", scroll);
+document.getElementById("lesMer").addEventListener("click", function() {progress=0});
