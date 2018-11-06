219db95b8290fa98a9a8e5e94add5c323a697b7b
Bendik Schrøder <bendik96@gmail.com>
2018-11-06 18:06:04 +0100

    Made header and footer as imported modules. Adding 'active' class to the nav links to the current page

---
+++ loadContent.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+
+  let header = document.querySelector('link[href="header.html"]').import.querySelector('header');
+  let footer = document.querySelector('link[href="footer.html"]').import.querySelector('footer');
+
+
+  document.querySelector('body').insertBefore(header, document.querySelector('body').firstChild);
+  document.querySelector('body').appendChild(footer);
+
+  let navItems = document.querySelectorAll('nav li a');
+  let pageName = window.location.pathname.split('/').pop();
+  for (let item of navItems) {
+    let href = item.href.split('/').pop();
+    if (href === pageName) {
+      item.classList.add('active');
+    }
+  }
+};
