d3a6f406590dddf9ccac2fa438139700e88f1b95
Bendik Schrøder <bendik96@gmail.com>
2018-11-08 10:43:53 +0100

    Fixed order calculation logic

--- js/prisanslag.js
+++ js/prisanslag.js
@@ -1,96 +1,92 @@
-const trestolper = document.getElementById('tre'),
-  dipp = document.getElementById('dipp'),
-  wood = document.getElementById('wood'),
-  regnut = document.getElementById('regnut'),
-  pris = document.getElementById('pris'),
-  plussD = document.getElementById('plussd'),
-  minusD = document.getElementById('minusd'),
-  plussW = document.getElementById('plussw'),
-  minusW = document.getElementById('minusw'),
-  plussT = document.getElementById('plusst'),
-  minusT = document.getElementById('minust'),
-  prisElem = document.getElementById('monthlyprice'),
-  engangspris = document.getElementById('engangspris'),
-  numberInput = [dipp, wood];
+const tre = document.getElementById('tre'),
+    dipp = document.getElementById('dipp'),
+    wood = document.getElementById('wood'),
+    regnut = document.getElementById('regnut'),
+    plussD = document.getElementById('plussd'),
+    minusD = document.getElementById('minusd'),
+    plussW = document.getElementById('plussw'),
+    minusW = document.getElementById('minusw'),
+    plussT = document.getElementById('plusst'),
+    minusT = document.getElementById('minust'),
+    prisElem = document.getElementById('monthlyprice'),
+    engangspris = document.getElementById('engangspris');
+
 let antTre, antDipp, antWood;
 
 // Anna sin scrolle-javascript her
-document.getElementById("regnut").addEventListener("click", function(){
-  setOffset(document.getElementById("pris1").offsetTop, window.pageYOffset);
-  scrollUntil()});
+document.getElementById('regnut').addEventListener('click', function() {
+  setOffset(document.getElementById('pris1').offsetTop, window.pageYOffset);
+  scrollUntil();
+});
+
 //Anna sin scrolle-javascript ferdig
 
-function checkInput() {
-  console.log(this.value);
-  if (this.value == '') {
-      this.value = '1';
-  }
-  else {
-    let n = parseInt(this.value);
-    if (n < 1) {
-        n = 1;
-    }
-    if (n > 99) {
-        n = 99;
-    }
-    this.value = n;
+function checkInput(inputelement) {
+  if (inputelement.value < 1) {
+    inputelement.value = 1;
+  } else if (inputelement.value > 99) {
+    inputelement.value = 99;
   }
 }
 
 function prisutregning() {
-  prisElem.innerHTML = parseInt(tre.value)*5;
-  engangspris.innerHTML = parseInt(dipp.value)*5000 + parseInt(wood.value)*10000;
+  prisElem.innerHTML = parseInt(tre.value) * 5;
+  engangspris.innerHTML = parseInt(dipp.value) * 5000 + parseInt(wood.value) * 10000;
 }
 
 plussW.addEventListener('click', function() {
   antWood = parseInt(wood.value);
   antWood += 1;
   wood.value = antWood;
-  checkInput.call(wood);
-})
+  checkInput(wood);
+});
 
 minusW.addEventListener('click', function() {
   antWood = parseInt(wood.value);
   antWood -= 1;
   wood.value = antWood;
-  checkInput.call(wood);
-})
+  checkInput(wood);
+});
 
 plussD.addEventListener('click', function() {
   antDipp = parseInt(dipp.value);
   antDipp += 1;
   dipp.value = antDipp;
-  checkInput.call(dipp);
-})
+  checkInput(dipp);
+});
 
 minusD.addEventListener('click', function() {
   antDipp = parseInt(dipp.value);
   antDipp -= 1;
   dipp.value = antDipp;
-  checkInput.call(dipp);
-})
+  checkInput(dipp);
+});
 
 plussT.addEventListener('click', function() {
   antTre = parseInt(tre.value);
   antTre += 10000;
   tre.value = antTre;
-  checkInputTre.call(tre);
-})
+});
 
 minusT.addEventListener('click', function() {
   antTre = parseInt(tre.value);
   antTre -= 10000;
-  tre.value = antTre;
-  checkInputTre.call(tre);
-})
 
-for (inp of numberInput) {
-    inp.addEventListener('input', checkInput);
-}
+  tre.value = antTre;
+  if (tre.value < 10000) {
+    tre.value = 10000;
+  }
+});
+
+tre.addEventListener('change', function() {
+  if (tre.value < 10000) {
+    tre.value = 10000;
+  }
+});
 
 regnut.addEventListener('click', function() {
   prisutregning();
-  scroll()
-})
+  scroll();
+});
 
 
