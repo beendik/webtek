c413fa36f94d3445b46d816bf2866de3fdb35af9
Bendik Schrøder <bendik96@gmail.com>
2018-11-08 11:36:20 +0100

    Added explaining comment about localhost, removed unnecessary async tag

--- js/loadContent.js
+++ js/loadContent.js
@@ -1,9 +1,11 @@
+// Link rel import with html only works on a server, so run this through localhost!
+
 let header = document.querySelector('link[href="header.html"]').import.querySelector('header');
 let footer = document.querySelector('link[href="footer.html"]').import.querySelector('footer');
 
 document.querySelector('body').insertBefore(header, document.querySelector('body').firstChild);
 
-window.onload = async function() {
+window.onload = function() {
   document.querySelector('body').appendChild(footer);
   let navItems = document.querySelectorAll('nav li a');
   let pageName = window.location.pathname.split('/').pop();
